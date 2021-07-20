da8569b8855a3f2c712e035eecdb1d3110951a0f
Burdeina Tetiana <44652791+burdeinatetiana@users.noreply.github.com>
2021-07-20 23:10:16 +0300

    First api tests (#1)
    * first API tests
    
    * update spec path in config

---
+++ tests/auth.spec.js
@@ -0,0 +1,26 @@
+import {expect} from "chai";
+import supertest from 'supertest';
+
+describe('auth', () => {
+    const request = supertest('https://paysis.herokuapp.com');
+
+    it('successful log in',() =>{
+    request
+        .post('/auth')
+        .send({login:'adminius', password: 'supers3cret'})
+        .end(function (err, res) {
+            expect(res.statusCode).to.eq(200);
+            expect(res.body.token).not.to.be.undefined;
+        });
+    });
+
+    it('log in with wrong credentials should return error', function (){
+      request
+          .post('/auth')
+          .send({login: 'wrong', password: 'wrong'})
+          .end(function (err, res) {
+              expect(res.statusCode).to.eq(404);
+              expect(res.body.message).to.eq('Wrong login or password.');
+          });
+    })
+});
